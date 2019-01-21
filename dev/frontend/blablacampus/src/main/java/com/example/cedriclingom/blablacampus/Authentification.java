2caed20067f8b5028ae4e80ede38488776937e82
cedriclingom <cedriclingom@gmail.com>
2019-01-21 14:39:10 +0100

    interface authentification

---
+++ dev/frontend/blablacampus/src/main/java/com/example/cedriclingom/blablacampus/Authentification.java
@@ -0,0 +1,28 @@
+package com.example.cedriclingom.blablacampus;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Authentification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+
+
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.blablaCampuspurple));
+    }
+}
